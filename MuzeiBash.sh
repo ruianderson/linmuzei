cc6045c00f5242fa10a3f63c01f1b6afcbb61967
Feminist-Software-Foundation <feministsoftwarefoundation@loves.dicksinmyan.us>
2014-02-22 15:18:10 +0000

    Re-structured script with comments; now supports non-gnome-settings-daemon-based sessions with feh; updated README

--- MuzeiBash.sh
+++ MuzeiBash.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+######Initial stuff######
 muzeiDir=~/Pictures/Muzei
 mkdir -p $muzeiDir/Wallpaper
 cd $muzeiDir
+
+######Get the Muzei JSON and parse it######
 curl -o muzei.json 'https://muzeiapi.appspot.com/featured?cachebust=1'
 imageUri=`jq '.imageUri' $muzeiDir/muzei.json | sed s/\"//g`
 imageFile=`basename $imageUri`
+title=`jq '.title' $muzeiDir/muzei.json | sed s/\"//g`
+byline=`jq '.byline' $muzeiDir/muzei.json | sed s/\"//g`
+
+######Get the latest wallpaper######
 cd Wallpaper
 if [ -f $imageFile ];
 then
@@ -13,10 +20,19 @@ else
   echo "File $imageFile does not exist, downloading..."
   curl -O $imageUri
 fi
-gsettings set org.gnome.desktop.background picture-uri file://$muzeiDir/Wallpaper/$imageFile
+
+######Set the wallpaper######
+if [ "$pidof gnome-settings-daemon)" ]
+then
+  echo "Gnome-settings-daemons detected, setting wallpaper with gsettings..."
+  gsettings set org.gnome.desktop.background picture-uri file://$muzeiDir/Wallpaper/$imageFile
+else
+  echo "Gnome-settings-daemons not running, setting wallpaper with feh..."
+  feh --bg-fill $muzeiDir/Wallpaper/$imageFile
+fi
+
+######Send a notification######
 cd $muzeiDir
-title=`jq '.title' $muzeiDir/muzei.json | sed s/\"//g`
-byline=`jq '.byline' $muzeiDir/muzei.json | sed s/\"//g`
 if [ -f MuzeiLogo.png ];
 then
   echo "Logo already exists."
@@ -26,5 +42,6 @@ else
 fi
 notify-send "New wallpaper: '$title'" "$byline" -i $muzeiDir/MuzeiLogo.png
 
+######Clean up old wallpapers######
 echo "Cleaning up old files..."
 find $muzeiDir/Wallpaper -ctime +30 | xargs rm -rf
--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Port of [Muzei](https://github.com/romannurik/muzei/) to Bash.
 
 Set this script as a(n) (ana)cronjob, and it'll download a new "famous work of art" everyday and set it as your wallpaper.
 
-Currently supports Linux running in a Gnome-based environment (sets wallpaper via gsettings).  Project is still new, so we don't support blurring or dimming yet (though I don't think those are useful for desktop, unlike smartphones and tablets).
+Currently supports Linux running in a Gnome-settings-daemon-based environment (Gnome-Shell, Unity, Pantheon, etc.; sets wallpaper via gsettings).  If Gnome-settings-daemon is not running, background is set using [feh](http://feh.finalrewind.org/).  Project is still new, so we don't support blurring or dimming yet (though I don't think those are useful for desktop, unlike smartphones and tablets).
 
 Requirements
 ------------
@@ -13,3 +13,4 @@ Requirements
 * Bash (duh!)
 * jq (for parsing Muzei's JSON; can be found in Debian, Ubuntu, Arch AUR, Gentoo, and Fedora repos.  If it's not in your distro's repos, check [here](http://stedolan.github.io/jq/download/).)
 * notify-send (for sending "wallpaper changed" notifications)
+* feh (only if you aren't running gnome-settings-daemon)
