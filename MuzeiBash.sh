08029a34d586c80f532b8faa17d40497bac8f20a
Feminist-Software-Foundation <feministsoftwarefoundation@loves.dicksinmyan.us>
2014-02-22 14:58:40 +0000

    Added notification for wallpaper change; store wallpapers in their own directory.

--- MuzeiBash.sh
+++ MuzeiBash.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 muzeiDir=~/Pictures/Muzei
-mkdir -p $muzeiDir
+mkdir -p $muzeiDir/Wallpaper
 cd $muzeiDir
 curl -o muzei.json 'https://muzeiapi.appspot.com/featured?cachebust=1'
 imageUri=`jq '.imageUri' $muzeiDir/muzei.json | sed s/\"//g`
 imageFile=`basename $imageUri`
+cd Wallpaper
 if [ -f $imageFile ];
 then
-   echo "File $imageFile exists."
+  echo "File $imageFile exists."
 else
-   echo "File $imageFile does not exist, downloading..."
-   curl -O $imageUri
+  echo "File $imageFile does not exist, downloading..."
+  curl -O $imageUri
 fi
-gsettings set org.gnome.desktop.background picture-uri file://$muzeiDir/$imageFile
+gsettings set org.gnome.desktop.background picture-uri file://$muzeiDir/Wallpaper/$imageFile
+cd $muzeiDir
+title=`jq '.title' $muzeiDir/muzei.json | sed s/\"//g`
+byline=`jq '.byline' $muzeiDir/muzei.json | sed s/\"//g`
+if [ -f MuzeiLogo.png ];
+then
+  echo "Logo already exists."
+else
+  echo "Lodo doesn't exist, downloading..."
+  curl -O "https://raw.github.com/Feminist-Software-Foundation/Muzei-Bash/master/MuzeiLogo.png"
+fi
+notify-send "New wallpaper: '$title'" "$byline" -i $muzeiDir/MuzeiLogo.png
+
 echo "Cleaning up old files..."
-find $muzeiDir -type d -ctime +30 | xargs rm -rf
+find $muzeiDir/Wallpaper -ctime +30 | xargs rm -rf
