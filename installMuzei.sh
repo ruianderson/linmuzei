3a58f7d3bcd95b889b727bd826c6e61dcaf2f2de
aepirli <pirlialperen@yandex.com>
2014-03-01 21:02:18 +0200

    cronjob update

--- installMuzei.sh
+++ installMuzei.sh
@@ -6,5 +6,5 @@ cp ./MuzeiBash.sh ~/.bin/
 cp ./checkMuzei.sh ~/.bin/
 chmod +x ~/.bin/MuzeiBash.sh
 chmod +x ~/.bin/checkMuzei.sh
-{ crontab -l; echo "0 */1 * * * /home/$(whoami)/.bin/checkMuzei.sh"; } | crontab - # checking every hour because not all timezones are the same, also ~/bin would not work at crontabs afaik
+{ crontab -l; echo "0 */1 * * * /home/$(whoami)/.bin/checkMuzei.sh"; } | crontab - # checking every hour because not all timezones are the same, also ~/.bin would not work at crontabs afaik
 echo "Muzei-Bash installed."
