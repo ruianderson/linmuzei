f4e830d6450ea4c1eb60f0e93abde8ecadcb16f8
Alp Pirli <pirlialperen@yandex.com>
2014-03-02 19:44:44 +0200

    install dialogue

--- installMuzei.sh
+++ installMuzei.sh
@@ -2,6 +2,17 @@
 # Muzei-Bash install script
 # Please do not run as root, it automatically will ask you for permission.
 
+if [ "$(id -u)" == "0" ]
+then
+  echo "If you run the install script as root the cronjob will probably not work for your user."
+fi
+
+read -r -p "Install Muzei-Bash? [Y/n] " y_or_n
+case $y_or_n in
+  [nN][oO]|[nN])      echo "Exiting." ; exit ;;
+  *)                  echo "Continuing." ;;
+esac
+
 { crontab -l; echo "0 * * * * checkmuzei"; } | crontab -
 sudo bash << EOF
 cp ./MuzeiBash.sh /usr/bin/muzeibash
